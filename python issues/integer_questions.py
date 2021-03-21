6f243a5f26f58721dffc62c4a608f12b4fe13b85
Max <brain.crash36@gmail.com>
2021-03-21 18:51:56 +0300

    issue 4 resolved

--- python issues/integer_questions.py
+++ python issues/integer_questions.py
@@ -1,13 +1,29 @@
 #задача 1
-print(11 * 2 ** 2 - 13 / 4 + 7)
+#print(11 * 2 ** 2 - 13 / 4 + 7)
 # issue 2
-import sys
-a = sys.getsizeof (3**9090001) / (1024*1024)
+#import sys
+#a = sys.getsizeof (3**9090001) / (1024*1024)
 # issue 3
-def pos_add(c, d):
-   return abs(c + d)
-a = input("Vvedite a: ")
-b = input("Vvedite b: ")
-x = pos_add(int(a), int(b))
-print(x)
+#def pos_add(c, d):
+#   return abs(c + d)
+#a = input("Vvedite a: ")
+#b = input("Vvedite b: ")
+#x = pos_add(int(a), int(b))
+#print(x)
+
+#issue 4
+#def num_sum(sum):
+#    if isinstance(sum, int) and not isinstance(sum, bool): #Proverka prinadlezhnosti k klassu int i ne prinadlezhnosti k bool
+#        sum_to_str = str(abs(sum)) #preobrazovanie v stroku
+#        s = 0
+#        for i in sum_to_str: #iteraciya cherez stroku
+#            s += int(i)
+#        return s
+#   return "Chislo ne celoe"
+
+#print(num_sum(112))
+#print(num_sum(-1024))
+#print(num_sum(-10.24))
+#print(num_sum(-1012312424))
+
 
